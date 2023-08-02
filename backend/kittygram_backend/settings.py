71b77944352735d5479340387a940134d0530a4f
Maxim Patsyuk <78837452+maxuver@users.noreply.github.com>
2023-08-02 17:12:36 +0400

    Add allowed host

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'kjh3l83kjdsf')
 
 DEBUG = os.environ.get('DEBUG', False) == 'True'
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['158.160.76.223', 'kittygram.myvnc.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
